d6191a01791fc757d7e05b3403dcc6c93009ec11
Martin <subfader@gmail.com>
2024-10-10 14:12:10 +0200

    Update script.user.js

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -10,8 +10,8 @@
 // @require      https://raw.githubusercontent.com/mixes-wiki/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://raw.githubusercontent.com/mixes-wiki/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://raw.githubusercontent.com/mixes-wiki/userscripts/refs/heads/main/includes/youtube_funcs.js
-// @require      https://raw.githubusercontent.com/Subfader/userscripts/refs/heads/main/includes/global.js?v-TrackId.net_15
-// @resource     IMPORTED_CSS https://raw.githubusercontent.com/Subfader/userscripts/refs/heads/main/includes/global.css?v-TrackId.net_12
+// @require      https://raw.githubusercontent.com/mixes-wiki/userscripts/refs/heads/main/includes/global.js?v-TrackId.net_15
+// @resource     IMPORTED_CSS https://raw.githubusercontent.com/mixes-wiki/userscripts/refs/heads/main/includes/global.css?v-TrackId.net_12
 // @include      http*trackid.net*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
